33dea06d4dffb94d8a1bcae02b4cbeb00503b6fd
elmikitozer <mikayay@icloud.com>
2026-06-05 10:58:04 +0200

    fix: change site description

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next"
 
 export const metadata: Metadata = {
   description:
-    "Atelier de poterie et céramique à Paris 9e depuis 40 ans. Cours de modelage et tournage pour adultes et enfants, de mère en fille.",
+    "Atelier de modelage et céramique à Paris 9e depuis 40 ans. Cours de modelage et céramique pour adultes et enfants, de mère en fille.",
 }
 
 import HomeHero from "@/components/sections/home/HomeHero"
